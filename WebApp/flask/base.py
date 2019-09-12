7cf227c2221ce376cff8104d1b707eb0dc2306a7
sahil97 <sahil.sharma071997@gmail.com>
2019-09-12 21:12:11 +0530

    updated flask app for dynamic area added support to remove rows that don't contain words from word list

--- WebApp/flask/base.py
+++ WebApp/flask/base.py
@@ -7,6 +7,7 @@ import json
 import tabula
 import math
 from flask import jsonify
+import img_proc as img_proc
 
 
 def init(file):
@@ -36,39 +37,67 @@ def extract_data(new_filename):
 
     # Hardcoded stuff for now
 
-    templates = {
-        'page1': (204.91, 33.138, 811.816, 570.86),
-        'page2': (107.49, 36.113, 800.672, 563.43)
-    }
+#     templates = {
+#         'page1': (204.91, 33.138, 811.816, 570.86),
+#         'page2': (107.49, 36.113, 800.672, 563.43)
+#     }
     pages = 6
     default_columns = ['Post', 'Beskrivelse', 'Enh.', 'Mengde', 'Enhetspris', 'Sum']
+    area_temp = img_proc.extract_cords_from_pdf(new_filename)
+    word_list = [
+    'Type','Wood','material','sorting','colour', 'subfloor', 'underlay', 'others', 'Industri', 'eik',
+    'rustik', 'full spectre', 'betong', 'plast', 'priming', 'heltre', 'ask', 'natur', 'sparkel', 'ullpapp',
+    'sliping', '1-stav', 'lønn', 'select', 'gips', '2mm', 'lakk', '2-stav', 'lerk', 'edel', 'spon', '3mm', 'olje',
+    '3-stav', 'valnøtt', 'sauvage', 'avrettingsmasse', 'aquastop', 'behandlet', 'stavparkett', 'bøk', 
+    'markant', 'GRANAB', 'silencio', 'dim', '6-36mm', 'ubehandlet', 'plank', 'afrikansk eik','exclusive', 
+    'Nivell',' etafoam', 'børstet', 'fiskebein', 'furu', 'family', 'subfloor', 'gips', 'strukturert', 'lamell',
+    'gran', 'trend', 'lim', '1-lags','favorit', '2-lags', '3-lags', 'laminat', 'vinyl', 'vinylklikk'
+]
+    df_final = pd.DataFrame(columns = default_columns)
 
     df_final = pd.DataFrame(columns = default_columns)
 
-    for i in range(1, pages):
-
+    for i in range(1, len(area_temp)):
         if(i == 1):
-            df = tabula.read_pdf('MyExport.pdf', pages='1', area=templates['page1'])
+            df = tabula.read_pdf(new_filename, pages='1', area=area_temp[i-1])
+            for col in df.columns:
+                if((df[col].isnull().sum()/len(df))>0.98):
+                    df.drop([col], axis=1, inplace=True)
 
+            df.columns = default_columns
             # Error correction for column names
-            df['Post'] = df['Unnamed: 0']
-            df.drop(['Unnamed: 0'], axis=1, inplace=True)
+    #         df['Post'] = df['Unnamed: 0']
+    #         df.drop(['Unnamed: 0'], axis=1, inplace=True)
 
-            df['Sum'] = df['Unnamed: 7']
-            df.drop(['Unnamed: 7'], axis=1, inplace=True)
+    #         df['Sum'] = df['Unnamed: 7']
+    #         df.drop(['Unnamed: 7'], axis=1, inplace=True)
 
             df_final_temp = extract_columns(df)
             df_final = pd.concat([df_final, df_final_temp])
 
+
         elif(i > 1):
-            print(i)
-            df = tabula.read_pdf('MyExport.pdf', pages=i, area=templates['page1'])
+            df = tabula.read_pdf(new_filename, pages=i, area=area_temp[i-1])
+            for col in df.columns:
+                if('Sum' in list(df[col])):
+                    df.drop([col], axis=1, inplace=True)
+                elif((df[col].isnull().sum()/len(df))>0.98):
+                    df.drop([col], axis=1, inplace=True)
             df.columns = default_columns
 
             df_final_temp = extract_columns(df)
 
             df_final = pd.concat([df_final, df_final_temp])
-
+    df_final = df_final.reset_index()
+    for index, rows in df_final.iterrows():
+        drop = 1
+        for word in list(rows['Beskrivelse'].split(' ')):
+            if word in word_list:
+                drop = 0
+        if(drop != 0):
+            df_final.drop([index], axis=0, inplace=True)
+    df_final.drop(['index'], axis=1, inplace=True)
+            
     return df_final
 
 
@@ -125,7 +154,7 @@ def get_new_df(df, sorted_indexes):
             for j in range(i1, i2):
 
     # Adding a new line after
-                sentence_list[i].append(df[df.columns[1]][j] + " \n")
+                sentence_list[i].append(str(df[df.columns[1]][j]) + " \n")
 
     # Handling rest of the value columns
                 for k in range(2, len(df.columns)):
@@ -143,7 +172,7 @@ def get_new_df(df, sorted_indexes):
             for j in range(i1, i2):
 
     # Adding a new line after
-                sentence_list[i].append(df[df.columns[1]][j] + " \n")
+                sentence_list[i].append(str(df[df.columns[1]][j]) + " \n")
 
                 for k in range(2, len(df.columns)):
                     if(type(df[df.columns[k]][j]) == str):
