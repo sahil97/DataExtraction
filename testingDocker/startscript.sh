ab5ae4a33564abe1031216bc85a63dcf68d0199f
sahil97 <sahil.sharma071997@gmail.com>
2019-09-24 21:44:49 +0530

    using gunicorn to serve flask modified script to start web server on port

--- testingDocker/startscript.sh
+++ testingDocker/startscript.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+# Start the second process
+cd web && python3 -m http.server 5006 &
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start my_second_process: $status"
+  exit $status
+fi
+
 # Start the first process
-python3 flask/app.py &
+cd flask && chmod +x ./script.sh && ./script.sh
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start my_first_process: $status"
   exit $status
 fi
-
-# Start the second process
-python3 -m http.server 5006
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start my_second_process: $status"
-  exit $status
